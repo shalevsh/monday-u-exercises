e533749f7e4c17f790310e9ad025273351ce6288
Shalev Sharf <81631445+shalevsh@users.noreply.github.com>
2022-06-12 14:23:45 +0300

    start

--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,10 +1,59 @@
-// Implement the `Main` class here
+import  ItemManager  from "./ItemManager.js";
+class Main {
+  constructor() {
+   this.itemManager = new ItemManager();
+   this.addButton = document.getElementById("add-btn");
+   this.inputTaskName = document.getElementById("input-task");
+   this.sortButton = document.getElementById("sort-btn");
+   this.clearAllButton = document.getElementById("clear-btn");
+  }
 
+  init() {
+    this.ListenToAddItem()
+    this.listenToSortButton();
+    this.listenToClearAllTasks();
+
+   
+
+  }
+ 
+   ListenToAddItem(){
+      this.AddTaskByEnter();
+      this.addButton.addEventListener("click", () => {
+      this.itemManager.addItem(this.inputTaskName.value)
+      this.inputTaskName.value = "";
+      //Clear the input when a new item is added
+    });
+}
+listenToSortButton(){
+  this.sortButton.addEventListener("click", () => {
+    this.itemManager.sortList();
+})};
+listenToClearAllTasks(){
+  this.clearAllButton.addEventListener("click", () => {
+    this.itemManager.clearList();
+})};
+
+  AddTaskByEnter() {
+    this.inputTaskName.addEventListener("keypress", event => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        this.itemManager.addItem(this.inputTaskName.value)
+        this.inputTaskName.value = "";
+
+      }
+    });
+  }
+}
 
 const main = new Main();
 
 document.addEventListener("DOMContentLoaded", function () {
-    // you should create an `init` method in your class
-    // the method should add the event listener to your "add" button
-    main.init();
+  // you should create an `init` method in your class
+  // the method should add the event listener to your "add" button
+  main.init();
+  
+  
 });
+
+
